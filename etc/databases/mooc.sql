8f67dfc77a1b60df563b79c581f3fa856ae16d92
Santiago Gtto <gttosantiago@gmail.com>
2020-07-02 16:37:45 +0200

    CRUSH > WIP: Solve error on pipeline.

--- etc/databases/mooc.sql
+++ etc/databases/mooc.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `crushes` (
   `id` CHAR(36) NOT NULL,
-  `name` VARCHAR(255) NOT NULL
+  `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -22,6 +22,6 @@ CREATE TABLE `domain_events` (
 
 CREATE TABLE `backoffice_crushes` (
     `id` CHAR(36) NOT NULL,
-    `name` VARCHAR(255) NOT NULL
+    `name` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
